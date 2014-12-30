58b933d4352837cec2a7005fcdb0c1159e75de23
Karl Winter <karlwinter777@gmail.com>
2014-12-30 19:36:10 +0100

    Updated sending udpjson

--- lib/sflow/storage/storage.rb
+++ lib/sflow/storage/storage.rb
@@ -4,37 +4,14 @@ class SflowStorage
   def self.send_udpjson(sflow)
 
   #remap hash-keys with prefix "sflow_"
-      mappings = {"agent_address" => "agent_address",
-                  "sub_agent_id" => "sub_agent_id",
-                  "sampling_rate" => "sample_ratio",
-                  "i_iface_value" => "iif",
-                  "o_iface_value" => "oif",
-                  # "vlan_src" => "sflow_vlan_src",
-                  # "vlan_dst" => "sflow_vlan_dst",
-                  "ipv4_src" => "srcip",
-                  "ipv4_dst" => "dstip",
-                  "frame_length" => "packet_size",
-                  "frame_length_multiplied" => "total_data",
-                  "ip_type" => "ip_type",
-                  "tcp_src_port" => "tcp_srcport",
-                  "tcp_dst_port" => "tcp_dstport",
-                  "tcp_urg" => "tcp_urg",
-                  "tcp_ack" => "tcp_ack",
-                  "tcp_psh" => "tcp_psh",
-                  "tcp_rst" => "tcp_rst",
-                  "tcp_syn" => "tcp_syn",
-                  "tcp_fin" => "tcp_fin",
-                  "udp_src_port" => "udp_srcport",
-                  "udp_dst_port" => "udp_dstport",
-                  #"i_octets" => "sflow_i_octets",
-                  #"o_octets" => "sflow_o_octets",
-                  #"interface" => "sflow_interface",
-                  #"input_packets_error" => "sflow_input_packets_error",
-                  #"output_packets_error" => "sflow_output_packets_error"
+      # mappings = {
+      #     "agent_address" => "agent_address",
+      #     "sub_agent_id" => "sub_agent_id",
+      #     "sampling_rate" => "sample_ratio",
+      #     "frame_length" => "packet_size",
+      # }
 
-      }
-
-      prefixed_sflow = Hash[sflow.map {|k, v| [mappings[k], v] }]
+      # prefixed_sflow = Hash[sflow.map {|k, v| [mappings[k], v] }]
 
       # if sflow['i_iface_value'] and sflow['o_iface_value']
       #   i_iface_name = {"sflow_i_iface_name" => SNMPwalk.mapswitchportname(sflow['agent_address'],sflow['i_iface_value'])}
@@ -43,7 +20,7 @@ class SflowStorage
       #   prefixed_sflow.merge!(o_iface_name)
       # end
 
-      $logstash.send(prefixed_sflow.to_json, 0)
+      $logstash.send(sflow.to_json, 0)
 
   end
 
