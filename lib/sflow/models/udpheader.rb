24cc58bb5a8798ac9b81a6e7a8453045c3cb611f
Karl Winter <karlwinter777@gmail.com>
2014-12-30 19:32:42 +0100

    Updated udpheader model to fix spelling error

--- lib/sflow/models/udpheader.rb
+++ lib/sflow/models/udpheader.rb
@@ -1,6 +1,6 @@
 class UDPHeader
 
-  attr_reader :sndr_port,:dist_port,:packet_length,:checksum,
+  attr_reader :sndr_port,:dest_port,:packet_length,:checksum,
     :data_length,:lower
 
   def initialize(packet,offset=0,length=nil,lower=nil)
@@ -8,7 +8,7 @@ class UDPHeader
     @offset = offset
     header = packet.unpack("x#{offset}n4")
     @sndr_port = header[0]
-    @dist_port = header[1]
+    @dest_port = header[1]
     @packet_length = header[2]
     @checksum = header[3]
     @data_length = @packet_length - 8
@@ -27,7 +27,7 @@ class UDPHeader
     "" <<
     "UDP Header\n" <<
     "  Sender Port     : #{@sndr_port}\n" <<
-    "  Distication Port: #{@dist_port}\n" <<
+    "  Destination Port: #{@dest_port}\n" <<
     "  Packet Length   : #{@packet_length}\n" <<
     "  Checksum        : #{@checksum}\n" <<
     "  (Data Length)   : #{@data_length}"
